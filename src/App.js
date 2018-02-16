fcb75233dbe59b3b8921b6ac6497e6697d055bb2
alexlbr <alex.lobera@gmail.com>
2018-02-16 00:10:04 +0000

    simplifies css and layout

--- src/App.js
+++ src/App.js
@@ -45,8 +45,8 @@ class App extends Component {
       >
         {message.to === "you" && (
           <img
-            src={`images/${this.state.selectedUser.username}_lg.jpg`}
-            alt={`${this.state.selectedUser.username}`}
+            src={`images/${selectedUser.username}_lg.jpg`}
+            alt={`${selectedUser.username}`}
             className="avatar medium"
           />
         )}
@@ -61,111 +61,98 @@ class App extends Component {
 
     return (
       <div className="app">
-          <div className="top-bar">
-            <i class="icon fab fa-facebook-messenger" />
+        <div className="top-bar">
+          <i class="icon fab fa-facebook-messenger" />
+        </div>
+        <div className="messenger">
+          <div className="threads">
+            <div className="thread-bar">
+              <div className="info-bar-content">
+                <a onClick={this.showSettings}>
+                  <i className="icon fas fa-cog" />
+                </a>
+                Messenger
+                <a onClick={this.newMessage}>
+                  <i className="icon fas fa-edit" />
+                </a>
+              </div>
+            </div>
+            <ul className="thread-list">
+              {users.map((user, i) => (
+                <li
+                  key={i}
+                  onClick={() => this.selectUser(user)}
+                  className="preview"
+                >
+                  <img
+                    src={`images/${user.username}_lg.jpg`}
+                    alt={`${user.username}`}
+                    className="avatar large"
+                  />
+                  <div>
+                    <div className="user-name">{`${user.name.first} ${
+                      user.name.last
+                    }`}</div>
+                    <div className="message-preview">message preview</div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-
-          <div className="messenger">
-
-            <div className="threads">
-
-              <div className="thread-bar">
-                <div className="info-bar-content">
-                  <a onClick={this.showSettings}>
+          <div className="conversation">
+            <div className="conversation-bar">
+              <div className="title">
+                  {selectedUser.name &&
+                    `${selectedUser.name.first} ${selectedUser.name.last}`}
+              </div>
+              <div className="conversation-menu">
+                <i className="icon fas fa-phone" />
+                <i className="icon fas fa-video" />
+                <i className="icon fas fa-info-circle" />
+              </div>
+            </div>
+            <div className="conversation-content">
+              <div className="messages">
+                <div className="list">
+                  {styledConversation || <p>You have no messages</p>}
+                </div>
+                <div className="new-message">
+                  <input
+                    type="text"
+                    placeholder="Type your message..."
+                    className="message-box"
+                  />
+                  <button>Send</button>
+                </div>
+              </div>
+              <div className="user-detail">
+                <div className="user-detail-content">
+                  <div className="user">
+                    <div className="avatar large">
+                      <img
+                        src={`images/${selectedUser.username}_lg.jpg`}
+                        alt={`${selectedUser.username}`}
+                        className="avatar large"
+                      />
+                    </div>
+                    <div className="user-title">
+                      <div className="user-name">
+                        {selectedUser.name &&
+                          `${selectedUser.name.first} ${selectedUser.name.last}`}
+                      </div>
+                      <div className="last-active">Active 20m ago</div>
+                    </div>
+                  </div>
+                  <div>
                     <i className="icon fas fa-cog" />
-                  </a>
-                  Messenger
-                  <a onClick={this.newMessage}>
-                    <i className="icon fas fa-edit" />
-                  </a>
-                </div>
-              </div>
-              <ul className="thread-list">
-                {users.map((user, i) => (
-                  <li
-                    key={i}
-                    onClick={() => this.selectUser(user)}
-                    className="preview"
-                  >
-                    <img
-                      src={`images/${user.username}_lg.jpg`}
-                      alt={`${user.username}`}
-                      className="avatar large"
-                    />
-                    <div>
-                      <div className="user-name">{`${user.name.first} ${
-                        user.name.last
-                      }`}</div>
-                      <div className="message-preview">message preview</div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="conversation">
-              <div className="conversation-bar">
-                <div className="conversation-title">
-                  <div className="user-name">
-                    {selectedUser.name &&
-                      `${selectedUser.name.first} ${selectedUser.name.last}`}
-                  </div>
-                  <div className="last-active">Active 20m ago</div>
-                </div>
-                <div className="conversation-menu">
-                  <div>
-                    <i className="icon fas fa-phone" />
-                  </div>
-                  <div>
-                    <i className="icon fas fa-video" />
-                  </div>
-                  <div>
-                    <i className="icon fas fa-info-circle" />
                   </div>
                 </div>
-              </div>
-              <div className="conversation-content">
-                <div className="messages">
-                  <div className="list">
-                    {styledConversation || <p>You have no messages</p>}
-                  </div>
-                  <div className="new-message">
-                    <input
-                      type="text"
-                      placeholder="Type your message..."
-                      className="message-box"
-                    />
-                    <div className="send-button">Send</div>
-                  </div>
-                </div>
-                <div className="user-detail">
-                  <div className="user-detail-content">
-                    <div className="user">
-                      <div className="avatar large">
-                        <img
-                          src={`images/${selectedUser.username}_lg.jpg`}
-                          alt={`${selectedUser.username}`}
-                          className="avatar large"
-                        />
-                      </div>
-                      <div className="user-title">
-                        <div className="user-name">
-                          {selectedUser.name &&
-                            `${selectedUser.name.first} ${selectedUser.name.last}`}
-                        </div>
-                        <div className="last-active">Active 20m ago</div>
-                      </div>
-                    </div>
-                    <div>
-                      <i className="icon fas fa-cog" />
-                    </div>
-                  </div>
-                  <div className="options">Options</div>
-                  <div className="facebook-profile">Facebook Profile</div>
-                </div>
+                <div className="options">Options</div>
+                <div className="facebook-profile">Facebook Profile</div>
               </div>
             </div>
           </div>
+        </div>
       </div>
     )
   }
