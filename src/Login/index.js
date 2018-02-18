6c138b54888d2f0ef97625fd1115cc1f63ee9614
Michaela Hercogova <michaela.hercogova@gmail.com>
2018-02-18 13:59:09 +0000

    login page

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {FormGroup, FormControl, Button} from 'react-bootstrap'
+import "./Login.css"
 
-const Login = () => (
-  <h1>Log In!</h1>
-)
+class Login extends Component {
+    constructor() {
+        super()
+        //Here you'll need to set up the state of the component
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        // Hint: In order for this to work you'll need to use the withRouter HOC:
+        // https://reacttraining.com/react-router/web/api/withRouter
+        const { history } = this.props
+
+        if (this.state.username === 'react' && this.state.password === '1234') {
+            history.push('/')
+        }
+    }
+
+    handleChange = (name, event) => {
+        let change = {}
+        change[name] = event.target.value
+        //You need to set the change object in the state of the component
+    }
+
+    render () {
+        return (
+            <form className="form-signin">
+                <FormGroup>
+                    <h2 className="form-signin-heading">Please sign in</h2>
+                </FormGroup>
+
+                <FormGroup>
+                    <FormControl onChange={(event) => this.handleChange('email', event)} className="form-control" id="email" type="email" value="" placeholder="Enter email" />
+                    <FormControl className="form-control" id="password" type="password" value="" placeholder="Password" />
+                </FormGroup>
+
+                <Button bsSize="large" bsStyle="primary" block type="submit">Sign in</Button>
+            </form>
+        )
+    }
+}
 
 export default Login
