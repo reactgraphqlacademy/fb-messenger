6e2acfc5347774438197f2d0dfb054eae21af0ff
Adam Lancaster <lancaster28@hotmail.com>
2018-02-18 12:29:50 +0000

    adds react router 4 with a basic login page

---
+++ src/Login/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Login = () => (
+  <h1>Log In!</h1>
+)
+
+export default Login
---
+++ src/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import App from './App'
+import Login from './Login'
+
+const Root = () => (
+  <Router >
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>
+)
+
+export default Root
