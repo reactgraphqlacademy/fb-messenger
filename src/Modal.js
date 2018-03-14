3798f15e7560e3e69503918e12e7a315290e2757
Richard Moss <richard@englishtutorslive.com>
2018-03-14 17:22:17 +0000

    Modifies Modal

--- src/Modal.js
+++ src/Modal.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { Modal as BootstrapModal } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
-const Modal = (props) => ([
-  <BootstrapModal
-    key="modal"
-    backdrop={ true }
-    show={ props.show }
-    onHide={ props.toggleModal }
-  >
-    <h2 style={{ padding:"100px 40px" }}>Sorry, not implemented!</h2>
-  </BootstrapModal>,
-  props.children
-])
+const Modal = (props) => (
+  <div>
+    <BootstrapModal
+      key="modal"
+      backdrop={ true }
+      show={ props.show }
+      onHide={ props.toggleModal }
+    >
+      <h2 style={{ padding:"100px 40px" }}>Sorry, not implemented!</h2>
+    </BootstrapModal>
+    {props.children}
+  </div>
+)
 
 
 Modal.propTypes = {
