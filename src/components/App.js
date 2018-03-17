0318c2f667f36b5ba5a1a3ede440e79a5cd625ad
alexlbr <alex.lobera@gmail.com>
2018-03-17 10:36:13 +0000

    adds Profile content and navigations

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import TopBar from './Layout/TopBar'
 import Messenger from './Messenger'
 import Footer from './Layout/Footer'
 import Login from './Login'
+import Profile from './Profile'
 
 const App = () => (
   <div className="app">
@@ -14,7 +15,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/messages" component={Messenger} />
-      <Route path="/profile" component={Messenger} />
+      <Route path="/profile" component={Profile} />
       <Route component={NotFound}/>
     </Switch>
     <Footer />
--- src/components/Layout/TopBar.js
+++ src/components/Layout/TopBar.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const TopBar = (props) => (
   <div className="top-bar">
-    <i className="icon fab fa-facebook-messenger" />
-    <Link to="?" className={`username ${props.userPosition || 'right'}`}>
+    <Link to="/messages"><i className="icon fab fa-facebook-messenger" /></Link>
+    <Link to="/profile" className={`username ${props.userPosition || 'right'}`}>
      profile <img src="/images/default.jpg" />
     </Link>
   </div>
