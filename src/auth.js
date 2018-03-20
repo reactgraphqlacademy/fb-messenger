142f5c799ee46739d7f4403cea3c09b584555364
Alex Lobera <alex.lobera@gmail.com>
2018-03-20 23:49:54 +0000

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -7,6 +7,8 @@ export const getSession = () => {
     if (jwt) {
       const base64Url = jwt.split('.')[1]
       const base64 = base64Url.replace('-', '+').replace('_', '/')
+      // what is window.abot ?
+      // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/atob
       session = JSON.parse(window.atob(base64))
     }
   } catch (error) {
