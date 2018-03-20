ac5d5923117baee1027fe4e4b2ee9aff33b70b55
alexlbr <alex.lobera@gmail.com>
2018-03-20 17:00:56 +0000

    adds simple api to authenticate

---
+++ src/auth.js
@@ -0,0 +1,17 @@
+import Cookies from 'js-cookie'
+
+export const getSession = () => {
+  const jwt = Cookies.get('__session')
+  let session = {}
+  try {
+    if (jwt) {
+      const base64Url = jwt.split('.')[1]
+      const base64 = base64Url.replace('-', '+').replace('_', '/')
+      session = JSON.parse(window.atob(base64))
+    }
+  } catch (error) {
+    console.log(error)
+  }
+
+  return session
+}
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,46 +1,69 @@
-import React, {Component} from 'react'
-import {FormGroup, FormControl, Button} from 'react-bootstrap'
+import React, { Component } from 'react'
+import { FormGroup, FormControl, Button } from 'react-bootstrap'
+import { withRouter } from 'react-router-dom'
+
 import './Login.css'
 
 class Login extends Component {
-    constructor() {
-        super()
-        //Here you'll need to set up the state of the component
+  constructor () {
+    super()
+    this.state = {
+      email: '',
+      password: ''
     }
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        // Hint: In order for this to work you'll need to use the withRouter HOC:
-        // https://reacttraining.com/react-router/web/api/withRouter
-        const { history } = this.props
+  handleChange = (name, event) => {
+    let change = {}
+    change[name] = event.target.value
+    this.setState(change)
+  }
 
-        if (this.state.username === 'react' && this.state.password === '1234') {
-            history.push('/')
-        }
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    const { history } = this.props
+    const { password, email } = this.state
+    const { status } = await fetch('/api/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ password, email })
+    })
+
+    if (status === 200) {
+      history.push('/')
     }
+  }
 
-    handleChange = (name, event) => {
-        let change = {}
-        change[name] = event.target.value
-        //You need to set the change object in the state of the component
-    }
+  render () {
+    return (
+      <form className="form-signin">
+        <FormGroup>
+          <h2 className="form-signin-heading">Please sign in</h2>
+        </FormGroup>
 
-    render () {
-        return (
-            <form className="form-signin">
-                <FormGroup>
-                    <h2 className="form-signin-heading">Please sign in</h2>
-                </FormGroup>
+        <FormGroup>
+          <FormControl
+            className="form-control"
+            id="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange.bind(this, 'email')}
+            placeholder="Enter email" />
+          <FormControl
+            className="form-control"
+            id="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange.bind(this, 'password')}
+            placeholder="Password" />
+        </FormGroup>
 
-                <FormGroup>
-                    <FormControl onChange={(event) => this.handleChange('email', event)} className="form-control" id="email" type="email" value="" placeholder="Enter email" />
-                    <FormControl className="form-control" id="password" type="password" value="" placeholder="Password" />
-                </FormGroup>
-
-                <Button bsSize="large" bsStyle="primary" block type="submit">Sign in</Button>
-            </form>
-        )
-    }
+        <Button bsSize="large" bsStyle="primary" block type="submit" onClick={this.handleSubmit}>Sign in</Button>
+      </form>
+    )
+  }
 }
 
 export default Login
