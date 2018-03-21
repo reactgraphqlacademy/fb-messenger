4c1ba5feda4fb1401753f610f793e832c5ed34e8
alexlbr <alex.lobera@gmail.com>
2018-03-21 20:56:13 +0000

    refactores Input

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-class Input extends Component {
-  constructor(props) {
-    super(props)
+const Input = ({ type, placeholder, defaultValue, onChange, value }) => (
+  <input
+    type={type || 'text'}
+    placeholder={placeholder || ''}
+    className="form-control"
+    defaultValue={defaultValue}
+    value={value}
+    onChange={onChange}
+  />
+)
 
-    this.state = { value: null }
-  }
-
-  render() {
-    const { onChange, type, placeholder, defaultValue } = this.props
-
-    return (
-      <input
-        type={type || 'text'}
-        placeholder={placeholder || ''}
-        className="form-control"
-        value={this.state.value || defaultValue || ''}
-        onChange={ e => {
-          const { target: { value }} = e
-          this.setState({ value })
-          onChange && onChange(e)
-        }}
-      />
-    )
-  }
-}
+// class Input extends Component {
+//   constructor(props) {
+//     super(props)
+//
+//     this.state = { value: null }
+//   }
+//
+//   render() {
+//     const { onChange, type, placeholder, defaultValue } = this.props
+//
+//     return (
+//       <input
+//         type={type || 'text'}
+//         placeholder={placeholder || ''}
+//         className="form-control"
+//         value={this.state.value || defaultValue || ''}
+//         onChange={ e => {
+//           const { target: { value }} = e
+//           this.setState({ value })
+//           onChange && onChange(e)
+//         }}
+//       />
+//     )
+//   }
+// }
 
 Input.propTypes = {
   onChange: PropTypes.func,
