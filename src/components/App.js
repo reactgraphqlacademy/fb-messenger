35842b9447a325e67ec0bd0a042d4a8a3203632d
Michaela Hercogova <michaela.hercogova@gmail.com>
2018-03-22 21:54:15 +1000

    readme file

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
+import styled from 'styled-components'
 
 import NotFound from './NotFound'
 import Home from './Home'
 import TopBar from './Layout/TopBar'
 import Messenger from './Messenger'
 import Footer from './Layout/Footer'
-import Login from './Login'
 import Profile from './Profile'
 
+const AppWrapper = styled.div`
+  overflow: hidden;
+  flex: 1;
+  flex-flow: row wrap;
+  flex-wrap: nowrap;
+  align-items: stretch;
+  justify-content: flex-start;
+  order: 0;
+  flex-flow: column nowrap;
+  align-items: stretch;
+  flex-direction: column;
+  display: flex;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+`
+
 const App = () => (
-  <div className="app">
+  <AppWrapper>
     <TopBar userPosition="right" />
     <Switch>
       <Route exact path="/" component={Home} />
@@ -19,7 +36,7 @@ const App = () => (
       <Route component={NotFound}/>
     </Switch>
     <Footer />
-  </div>
+  </AppWrapper>
 )
 
 export default App
