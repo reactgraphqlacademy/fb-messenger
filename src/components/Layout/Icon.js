2548d10624c7586a0d5024b192a6e520be3c0239
alexlbr <alex.lobera@gmail.com>
2018-03-28 20:22:08 +0100

    adds prop-types for StyledIcon

--- src/components/Layout/Icon.js
+++ src/components/Layout/Icon.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import colours from '../../styles/export/colours.css'
 
@@ -6,6 +7,10 @@ const StyledIcon = styled.i`
   color: ${props => props.active ? colours.lightBlue : colours.grey};
   font-size: ${props => `${props.size}rem`};
 `
+
+StyledIcon.propTypes = {
+  active: PropTypes.bool
+}
 
 const Icon = ({ name, size = 1.5, active, style, ...props }) => (
     <StyledIcon className={`fas fa-${name}`} size={size} active={active} style={style} {...props} />
