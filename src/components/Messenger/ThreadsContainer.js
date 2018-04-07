4284e2b714764b4ae074098428729c6f302139cc
alexlbr <alex.lobera@gmail.com>
2018-04-07 16:37:52 +0100

    adds hints about dispatch in ThreadsContainer

--- src/components/Messenger/ThreadsContainer.js
+++ src/components/Messenger/ThreadsContainer.js
@@ -28,6 +28,39 @@ class ThreadsContainer extends Component {
   }
 }
 
+/*
+SOME EXPLANATION AND TIPS ABOUT USING CONNECT
+
+// This is the simplest way to connect our component to the store.dispatch method
+// and probably not the best
+const mapDispatchToProps = dispatch => ({
+  dispatch
+})
+
+// We can also connect directly an action with dispatch by doing this.
+// This is nice because you provide the component just with the actions it can dispatch
+// You might think that it's maybe a lot of typing, and you might be right.
+const mapDispatchToProps = dispatch => ({
+  dispatchReceiveThread: thread => {
+    dispatch(actions.receiveThread(thread))
+  }
+})
+
+// Then instead of this
+this.props.dispatch(actions.receiveThread(thread))
+// we can simply do this
+this.props.dispatchReceiveThread(thread)
+
+// The following technique is not implemented in our simplified react-redux,
+// but { connect } from 'react-redux' does. It helps get the best of the previous ones:
+// Not much typing, and we just provide to the component the actions it can dispatch
+const mapDispatchToProps = {
+  dispatchReceiveThread: actions.receiveThread
+}
+
+// try to use the last one :)
+*/
+
 const mapStateToProps = (state) => ({
   thread: state.thread
 })
