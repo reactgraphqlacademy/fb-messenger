8ee8b3efe6b9cdd5a938d94e94bae6c32e05fb83
Horacio Herrera <hherrerag@me.com>
2018-04-29 16:13:24 +0100

    remove withRouter from TopBar (no needed)

--- src/components/Layout/TopBar.js
+++ src/components/Layout/TopBar.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
 import { logOut, getSession } from '../../auth'
 
-const TopBar = (props) => {
+const TopBar = props => {
   const session = getSession()
 
   return (
     <div className="top-bar">
-      <Link to="/messages"><i className="icon fab fa-facebook-messenger" /></Link>
+      <Link to="/messages">
+        <i className="icon fab fa-facebook-messenger" />
+      </Link>
       <ul className={`${props.userPosition || 'right'}`}>
         <li>
-          <Link to="/login" onClick={logOut}>log out</Link>
+          <Link to="/login" onClick={logOut}>
+            log out
+          </Link>
         </li>
         <li>
           <Link to="/profile">
-            {session? session.username: ''} <img src="/images/default.jpg" />
+            {session ? session.username : ''} <img src="/images/default.jpg" />
           </Link>
         </li>
       </ul>
@@ -23,4 +26,4 @@ const TopBar = (props) => {
   )
 }
 
-export default withRouter(TopBar)
+export default TopBar
