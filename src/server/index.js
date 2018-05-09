ece62aeea58ac6abbec1a2aab06b75a8e05a6570
alexlbr <alex.lobera@gmail.com>
2018-05-09 20:54:37 +0100

    removes comments render.js

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,8 @@ import render from './render'
 
 const server = express()
 
+// The following middleware is to proxy some paths from the WebpackDevServer to
+// the server that renders React. This is just for development.
 server.use(
   ['/static', '/sockjs-node'],
   proxy({
@@ -30,32 +32,3 @@ const serverInstance = server.listen(process.env.REACT_APP_SERVER_PORT, () => {
   console.log(`Environment = ${process.env.NODE_ENV}`)
   console.log(`Running an Express server at http://${address}:${port}`)
 })
-
-
-
-// import express, { Router } from 'express'
-// import path from 'path'
-// import proxy from 'http-proxy-middleware'
-// import setupServer from './setupServer'
-// import createFakeAPI from './api'
-//
-// const middlewares = [
-//   Router()
-//     // this should be in a different project, it's hear to emulate an API
-//     .use(createFakeAPI())
-//     .use('/static/css/', express.static(path.join(process.cwd(), 'src/shared/styles')))
-//     .use(['/static', '/sockjs-node'],
-//       proxy({
-//         target: `http://localhost:${process.env.REACT_APP_CLIENT_PORT}`,
-//         ws: true,
-//         logLevel: 'error'
-//       }))
-// ]
-//
-// const app = setupServer({ middlewares })
-//
-// const server = app.listen(process.env.REACT_APP_SERVER_PORT, () => {
-//   const { address, port } = server.address()
-//   console.log(`Environment = ${process.env.NODE_ENV}`)
-//   console.log(`Running an Express server at http://${address}:${port}`)
-// })
