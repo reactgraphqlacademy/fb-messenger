5bae13123a808dd052f59f4d268c7d174a56990e
Horacio Herrera <hherrerag@me.com>
2018-06-12 17:56:08 +0200

    change StaticRouter to BrowserRouter

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import React from 'react'
-import { StaticRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import proxy from 'http-proxy-middleware'
 
 import Root from '../shared/components/Root'
@@ -15,7 +15,7 @@ server.use(
   ['/static', '/sockjs-node'],
   proxy({
     target: `http://localhost:${process.env.REACT_APP_CLIENT_PORT}`,
-    ws:true,
+    ws: true
   })
 )
 
