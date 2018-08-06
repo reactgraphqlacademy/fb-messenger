115370b9d4e3e04dd6021359d34559facbc7a3fc
alexlbr <alex.lobera@gmail.com>
2018-08-06 13:47:37 +0200

    adds comment in thread.test.js

--- src/actions/thread.test.js
+++ src/actions/thread.test.js
@@ -4,8 +4,8 @@ import {
 } from './thread'
 
 describe('Thread action', () => {
-  // 1- pair up 3 min and discuss wich statement you are going to write to describe the test case
-  it(``, () => {
-
+  // 1- pair up and discuss wich statement you are going to write to describe the test case
+  it(`should have a type`, () => {
+      //2- Write a test for the function receiveThread 
   })
 })
