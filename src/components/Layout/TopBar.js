e1e1145dad02eb1191e60fa82e81fbff8c4be823
alexlbr <alex.lobera@gmail.com>
2018-08-21 12:44:08 +0100

    updates Topbar

--- src/components/Layout/TopBar.js
+++ src/components/Layout/TopBar.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { logOut, getSession } from '../../auth'
 
-const TopBar = (props) => (
-  <div className="top-bar">
-    <Link to="/messages"><i className="icon fab fa-facebook-messenger" /></Link>
-    <Link to="/profile" className={`username ${props.userPosition || 'right'}`}>
-     profile <img src="/images/default.jpg" />
-    </Link>
-  </div>
-)
+const TopBar = props => {
+  const session = getSession()
+
+  return (
+    <div className="top-bar">
+      <Link to="/messages">
+        <i className="icon fab fa-facebook-messenger" />
+      </Link>
+      <ul className={`${props.userPosition || 'right'}`}>
+        <li>
+          <Link to="/login">
+            log out
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile">
+            {session ? session.username : ''} <img src="/images/default.jpg" />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
 export default TopBar
