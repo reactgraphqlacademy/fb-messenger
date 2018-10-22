c1dabafb12522584733c8e314ec8ed2de69303e6
alexlbr <alex.lobera@gmail.com>
2018-10-22 14:04:06 +0100

    updates store.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,28 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
 
-const addLoggerMiddleware = store => {
-  const rawDispatch = store.dispatch
-  return (action) => {
-    console.group(action.type)
-    console.log('prev state', store.getState())
-    console.log('action', action)
-    const returnValue = rawDispatch(action)
-    console.log('next state', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
+const addLoggerMiddleware = store => next => action => {
+  console.group(action.type)
+  console.log('prev state', store.getState())
+  console.log('action', action)
+  const returnValue = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return returnValue
 }
 
-const addThunkMiddleware = store => {
-  const rawDispatch = store.dispatch
-  return action => {
-    if (typeof action === 'function') {
-      return action(store.dispatch, store.getState)
-    }
-    return rawDispatch(action)
-  };
+const addThunkMiddleware = store =>  next => action => {
+  if (typeof action === 'function') {
+    return action(store.dispatch, store.getState)
+  }
+  return next(action)
 };
 
 const configureStore = () => {
   const store = createStore(
-    reducers
+    reducers,
+    applyMiddleware(addLoggerMiddleware,addThunkMiddleware)
   )
-
-  store.dispatch = addLoggerMiddleware(store)
-  store.dispatch = addThunkMiddleware(store)
 
   return store
 }
