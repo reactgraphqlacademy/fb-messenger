716319032373255de0a1582936a8eb637ae863d0
alexlbr <alex.lobera@gmail.com>
2018-11-01 15:01:09 -0400

    updates Form/Input in forms & auth

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,16 +1,52 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// Option 1
 const Input = ({ type, placeholder, defaultValue, onChange, value }) => (
   <input
     type={type || 'text'}
     placeholder={placeholder || ''}
     className="form-control"
-    defaultValue={defaultValue}
     value={value}
+    defaultValue={defaultValue}
     onChange={onChange}
   />
 )
+
+// Option 2
+// const Input = (props) => (
+//   <input
+//     {...props}
+//     className="form-control"
+//   />
+// )
+
+// Option 3
+// class Input extends Component {
+//   constructor(props) {
+//     super(props)
+//
+//     this.state = { value: null }
+//   }
+//
+//   render() {
+//     const { onChange, type, placeholder, defaultValue } = this.props
+//
+//     return (
+//       <input
+//         type={type || 'text'}
+//         placeholder={placeholder || ''}
+//         className="form-control"
+//         value={this.state.value || defaultValue || ''}
+//         onChange={ e => {
+//           const { target: { value }} = e
+//           this.setState({ value })
+//           onChange && onChange(e)
+//         }}
+//       />
+//     )
+//   }
+// }
 
 Input.propTypes = {
   onChange: PropTypes.func,
