b0bc877e4dea1a2f7ec0395a4491f24b1b616785
alexlbr <alex.lobera@gmail.com>
2018-12-04 12:23:54 +0000

    login api accepts pasword === instead of ==

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
-const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
-const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
-const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
-const ignoredFiles = require('react-dev-utils/ignoredFiles');
-const config = require('./webpack.config.dev');
-const paths = require('./paths');
+const errorOverlayMiddleware = require("react-dev-utils/errorOverlayMiddleware");
+const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
+const ignoredFiles = require("react-dev-utils/ignoredFiles");
+const config = require("./webpack.config.dev");
+const paths = require("./paths");
 
-const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
-const host = process.env.HOST || '0.0.0.0';
+const protocol = process.env.HTTPS === "true" ? "https" : "http";
+const host = process.env.HOST || "0.0.0.0";
 
 module.exports = function(proxy, allowedHost) {
   return {
@@ -31,12 +31,12 @@ module.exports = function(proxy, allowedHost) {
     // specified the `proxy` setting. Finally, we let you override it if you
     // really know what you're doing with a special environment variable.
     disableHostCheck:
-      !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
+      !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === "true",
     // Enable gzip compression of generated files.
     compress: true,
     // Silence WebpackDevServer's own logs since they're generally not useful.
     // It will still show compile warnings and errors with this setting.
-    clientLogLevel: 'none',
+    clientLogLevel: "none",
     // By default WebpackDevServer serves physical files from current directory
     // in addition to all the virtual build products that it serves from memory.
     // This is confusing because those files won’t automatically be available in
@@ -71,43 +71,38 @@ module.exports = function(proxy, allowedHost) {
     // src/node_modules is not ignored to support absolute imports
     // https://github.com/facebookincubator/create-react-app/issues/1065
     watchOptions: {
-      ignored: ignoredFiles(paths.appSrc),
+      ignored: ignoredFiles(paths.appSrc)
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
-    https: protocol === 'https',
+    https: protocol === "https",
     host: host,
     overlay: false,
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
       // See https://github.com/facebookincubator/create-react-app/issues/387.
-      disableDotRule: true,
+      disableDotRule: true
     },
     public: allowedHost,
     proxy,
     before(app) {
       // Middleware to simulate an authentication API
-      app.use('/api/auth',
-        bodyParser.json(),
-        (req, res) => {
-          const { email, password } = req.body
-          if (email === 'clone@facebook.com' && password === '123') {
-            const SEVEN_DAYS_IN_MILLISECONDS = 604800000
-            const cookie = jwt.sign(
-              { id: '5ab1299177282be8578f3612', username: '@theclone' },
-              'this_is_my_secret_key ^^',
-              { expiresIn: '7 days' }
-            )
-            res.cookie(
-              '__session',
-              cookie,
-              { maxAge: SEVEN_DAYS_IN_MILLISECONDS }
-            )
-            res.status(200).send('Authorized')
-          } else {
-            res.status(401).send('Not authorized')
-          }
+      app.use("/api/auth", bodyParser.json(), (req, res) => {
+        const { email, password } = req.body;
+        if (email === "clone@facebook.com" && password == "123") {
+          const SEVEN_DAYS_IN_MILLISECONDS = 604800000;
+          const cookie = jwt.sign(
+            { id: "5ab1299177282be8578f3612", username: "@theclone" },
+            "this_is_my_secret_key ^^",
+            { expiresIn: "7 days" }
+          );
+          res.cookie("__session", cookie, {
+            maxAge: SEVEN_DAYS_IN_MILLISECONDS
+          });
+          res.status(200).send("Authorized");
+        } else {
+          res.status(401).send("Not authorized");
         }
-      )
+      });
       // This lets us open files from the runtime error overlay.
       app.use(errorOverlayMiddleware());
       // This service worker file is effectively a 'no-op' that will reset any
@@ -116,6 +111,6 @@ module.exports = function(proxy, allowedHost) {
       // it used the same host and port.
       // https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
       app.use(noopServiceWorkerMiddleware());
-    },
+    }
   };
 };
