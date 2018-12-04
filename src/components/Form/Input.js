ac069a3f0ab6131eb6740a31822247a4ddf6e460
Alex Lobera <alex.lobera@gmail.com>
2018-12-04 15:53:50 +0000

    Update Input.js

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 /*
 
-Here you can create your uncontrolled Input.
+Here you can create your controlled and uncontrolled Input.
 
 Hints:
-- This component will be uncontrolled from the outer components, meaning this
-component needs to keep track of the state of the input.
-- Uncontrolled components receive a prop to "notify" the outer components that the
-state has changed. That prop is normally called onChange.
-- If you add the class form-control to your Input it will keep the style.
+- To make this component uncontrolled from the outer components the state should be managed internally somewhere in this component.
+- To make this component controlled it should receive a prop with the value to display in the input. 
+- If the Input receives the value prop then it should be controlled, if it doesn't receive the value prop then it should be uncontrolled.
+- You can add the class form-control to your Input to encapsulate the style. This way you can use 
+  <Input value={someValue} onChange={handleChange} /> and the style becomes an implementation detail of Input
 
 */
