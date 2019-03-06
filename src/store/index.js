ed3985756974d55dd52205bbff7ffbd2647e6615
Richard Moss <richard@englishtutorslive.com>
2019-03-06 12:29:32 +0000

    Add dev tools incase students want to use it and add further hint on confusing point

--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,5 @@ import { createStore } from 'redux'
 import reducers from '../reducers'
 
 export const configureStore = (initialState) => {
-  return createStore(reducers, initialState)
+  return createStore(reducers, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 }
