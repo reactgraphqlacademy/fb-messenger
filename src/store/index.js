d69316e4ed6ec9e8e44db8759542e5c1824245bb
Richard Moss <richard@englishtutorslive.com>
2019-03-06 12:39:54 +0000

    Add redux dev tools so it can be directly installed for those who want to use

--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,5 @@ import { createStore } from '../redux/createStore'
 import reducers from '../reducers'
 
 export const configureStore = (initialState) => {
-  return createStore(reducers, initialState)
+  return createStore(reducers, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 }
