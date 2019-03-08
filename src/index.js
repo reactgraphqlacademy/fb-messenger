abd4251705ce22d21b28689cdd9b301d2fa28224
alexlbr <alex.lobera@gmail.com>
2019-03-08 14:32:07 +0000

    adds css files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,46 @@ import { createHttpLink } from "apollo-link-http";
 import { ApolloLink } from "apollo-link";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
+import { injectGlobal } from "styled-components";
 
 import { configureStore } from "./App/store";
-import "./App/styles/index.css";
 import Root from "./App/components/Root";
+
+injectGlobal`
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  font-size: 100%;
+  height: 100%;
+}
+a:link,
+a:focus,
+a:hover {
+  cursor: pointer;
+}
+button {
+  color: #0084ff;
+  border: none;
+  cursor: pointer;
+  font-weight: bolder;
+}
+h2 {
+  font-weight: 300;
+  text-align: center;
+  margin: auto;
+  font-size: 1rem;
+}
+.page-center {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`;
 
 const configLink = {
   uri: `/graphql`,
