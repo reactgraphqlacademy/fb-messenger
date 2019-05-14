a6766ecc2bf0c334f4079f91ceab374fdbef1fe6
alexlbr <alex.lobera@gmail.com>
2019-05-14 16:45:34 +0100

    refactores Input

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,37 +1,5 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
 
-class Input extends Component {
-  constructor(props) {
-    super(props)
+const Input = props => <input {...props} className="form-control" />;
 
-    this.state = { value: null }
-  }
-
-  render() {
-    const { onChange, type, placeholder, defaultValue } = this.props
-
-    return (
-      <input
-        type={type || 'text'}
-        placeholder={placeholder || ''}
-        className="form-control"
-        value={this.state.value || defaultValue || ''}
-        onChange={ e => {
-          const { target: { value }} = e
-          this.setState({ value })
-          onChange && onChange(e)
-        }}
-      />
-    )
-  }
-}
-
-Input.propTypes = {
-  onChange: PropTypes.func,
-  placeholder: PropTypes.string,
-  type: PropTypes.string,
-  defaultValue: PropTypes.string,
-}
-
-export default Input
+export default Input;
