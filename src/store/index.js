c6b5a1164e547af5298cb082c8834b83d7a321b1
alexlbr <alex.lobera@gmail.com>
2019-05-14 17:16:11 +0100

    adds css

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-// import { createStore } from 'redux'
-import { createStore } from '../redux/createStore'
-import reducers from '../reducers'
+import { createStore } from "redux";
+import reducers from "../reducers";
 
-export const configureStore = (initialState) => {
-  return createStore(reducers, initialState)
-}
+export const configureStore = initialState => {
+  return createStore(reducers, initialState);
+};
