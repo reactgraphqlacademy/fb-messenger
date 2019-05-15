8518a8e206ab9b61b9c50ee8b7688476dbde5a5a
alexlbr <alex.lobera@gmail.com>
2019-05-15 12:01:50 +0100

    removes comments in src/reducers/thread.test.js

--- src/reducers/thread.test.js
+++ src/reducers/thread.test.js
@@ -2,15 +2,6 @@ import { receiveThread, RECEIVE_THREAD } from "../actions/thread";
 
 import threadReducer from "./thread";
 
-/*
-
-What BEHAVIOUR do we have to test?
-
-Task 1, write the describe and its
-Task 2, implement the expectations
-
-*/
-
 describe("Thread reducer", () => {
   it(`should return the default state if no state is provided`, () => {
     expect(threadReducer(undefined, {})).toMatchSnapshot();
