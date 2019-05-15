51d00236da50deb4c4f845694e4558796131a174
alexlbr <alex.lobera@gmail.com>
2019-05-15 10:55:19 +0100

    updates src/api/thread.test.js

--- src/api/thread.test.js
+++ src/api/thread.test.js
@@ -1,24 +1,22 @@
-import * as api from './thread'
+import * as api from "./thread";
 
-// DON'T DO THIS
+// DON'T TEST THE FOLLOWING. WHY? -> https://reactjs.academy/blog/unit-testing-fundamentals-explained-using-javascript/#mocking
 
-describe('#API thread endpoint', () => {
-  it('should fetchThreads', () => {
-    global.fetch = jest.fn()
+describe("#API thread endpoint", () => {
+  it("should fetchThreads", () => {
+    global.fetch = jest.fn();
 
-    fetch.mockImplementation(
-      (params) => Promise.resolve({
+    fetch.mockImplementation(params =>
+      Promise.resolve({
         json: () => ({
-          data,
+          data: [],
           params
         })
       })
-    )
-    api.fetchThreads().then((response) => {
-      expect(response.data).toEqual([])
-      expect(global.fetch).toBeCalledWith(
-        '/mocks/threads.json'
-      )
-    })
-  })
-})
+    );
+    api.fetchThreads().then(response => {
+      expect(response.data).toEqual([]);
+      expect(global.fetch).toBeCalledWith("/mocks/threads.json");
+    });
+  });
+});
