adcab70826b70008db3ba07f93415eb5d45495b0
Richard Moss <richard@englishtutorslive.com>
2019-06-25 14:41:32 +0200

    add basic form fill in test and automatic type aquisition

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+describe("Login", function() {
+  it("clicking '1-week bootcamp' navigates to a correct url", function() {
+    cy.visit("/login");
+
+    cy.get('input[type="email"]').type("clone@facebook.com");
+    cy.get('input[type="password"]').type("123");
+
+    cy.get("button").click();
+  });
+});
--- cypress/integration/navigation.spec.js
+++
@@ -1,9 +0,0 @@
-describe("Navigation", function() {
-  it("navigates to messenger", function() {
-    cy.visit("/login");
-
-    cy.contains("Messenger").click();
-
-    cy.url().should("include", "/messages");
-  });
-});
