08b498bf950704b09acb5381675630afbe060204
alexlbr <alex.lobera@gmail.com>
2019-08-22 16:44:56 +0100

    updates package.json and index.js and Messages.test.js

--- src/components/Messenger/Chat/Messages.test.js
+++ src/components/Messenger/Chat/Messages.test.js
@@ -4,6 +4,8 @@ import { MemoryRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import waitForExpect from "wait-for-expect";
 import { act } from "react-dom/test-utils";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent, wait } from "@testing-library/react";
 
 import { configureStore } from "../../../store";
 import ComposedMessages, { Messages, InputMessage, Message } from "./Messages";
@@ -52,13 +54,41 @@ describe("<Messages />", () => {
     // Questions:
     // - Is this black-box testing or white-box testing?
     // - If I remove Redux from my application and put all the state in React, do I need to update this test?
+    //    HEADS UP! implement a Root component
     // - What's your level of confidence that the user will be able to send a message?
   });
 
   it(`should send a message (integration test with React Testing Library)`, async () => {
-    // Questions:
-    // - Is this black-box testing or white-box testing?
-    // - If I remove Redux from my application and put all the state in React, do I need to update this test?
-    // - What's your level of confidence that the user will be able to send a message?
+    // const api = {
+    //   sendMessage: jest.fn(message => Promise.resolve(message))
+    // };
+    // const { queryAllByTestId, getByText, getByPlaceholderText } = render(
+    //   <Root>
+    //     <ComposedMessages api={api} username="alex_lobera" />
+    //   </Root>
+    // );
+    // // ***************************************************
+    // // HEADS UP! ADD data-testid={`message-${message.id}`}
+    // // ***************************************************
+    // expect(queryAllByTestId(/message-*./i).length).toBe(0);
+    // fireEvent.change(getByPlaceholderText(/Type your message.../i), {
+    //   target: { value: "Hi!" }
+    // });
+    // fireEvent.click(getByText(/Send/i));
+    // // https://testing-library.com/docs/dom-testing-library/api-async#wait
+    // await wait(() => {
+    //   const messages = queryAllByTestId(/message-*./i);
+    //   expect(messages.length).toBe(1);
+    //   expect(messages[messages.length - 1]).toHaveTextContent("Hi!");
+    // });
+    // // Questions:
+    // // - Is this black-box testing or white-box testing?
+    // // - If I remove Redux from my application and put all the state in React, do I need to update this test?
+    // // - What's your level of confidence that the user will be able to send a message?
   });
 });
+
+// const Root = ({ children }) => {
+//   const store = configureStore();
+//   return <Provider store={store}>{children}</Provider>;
+// };
