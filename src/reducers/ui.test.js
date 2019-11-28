bfc366b8e25ce3ac75ce646f0cb567ea20b5c3dd
alexlbr <alex.lobera@gmail.com>
2019-11-28 10:53:51 +0000

    update ui reducer test

--- src/reducers/ui.test.js
+++ src/reducers/ui.test.js
@@ -1,9 +1,6 @@
-import {
-  toggleMessageDetail,
-  TOGGLE_MESSAGE_DETAIL,
-} from '../actions/ui'
+import { toggleMessageDetail, TOGGLE_MESSAGE_DETAIL } from "../actions/ui";
 
-import uiReducer, { getInitialState } from './ui'
+import uiReducer, { getInitialState } from "./ui";
 
 /*
 
@@ -13,27 +10,28 @@ Task 1, write the describe and its
 Task 2, implement the expectations
 
 */
-describe('UI reducer', () => {
+describe("UI reducer", () => {
   it(`should return the default state if no state is provided`, () => {
-    const actualState = uiReducer(undefined, { type: "NO_RELEVANT_ACTION" })
-    const expectedState = getInitialState()
+    const actualState = uiReducer(undefined, { type: "NO_RELEVANT_ACTION" });
+    const expectedState = getInitialState();
 
-    expect(expectedState).toEqual(actualState)
-  })
+    expect(actualState).toEqual(expectedState);
+  });
 
   it(`should return a new state if it receives a ${TOGGLE_MESSAGE_DETAIL} action`, () => {
-    const actualState = uiReducer(undefined, toggleMessageDetail())
-    const expectedState = getInitialState()
-    expectedState.isMessageDetailOpen = true
+    const actualState = uiReducer(undefined, toggleMessageDetail());
+    const expectedState = { isMessageDetailOpen: true };
 
-    expect(expectedState).toEqual(actualState)
-  })
+    expect(actualState).toEqual(expectedState);
+  });
 
   it(`should return the current state if the action it receives it's not handled by the reducer`, () => {
-    const expectedState = getInitialState()
-    expectedState.isMessageDetailOpen = true
-    const actualState = uiReducer(expectedState, { type: "NO_RELEVANT_ACTION" })
+    const expectedState = getInitialState();
+    const actualState = uiReducer(expectedState, {
+      type: "NO_RELEVANT_ACTION"
+    });
+    actualState.isMessageDetailOpen = true;
 
-    expect(expectedState).toEqual(actualState)
-  })
-})
+    expect(actualState).toEqual(expectedState);
+  });
+});
