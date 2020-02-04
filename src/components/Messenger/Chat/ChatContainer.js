de37886a4c06b1446a0744de78fd23e20adb7705
alexlbr <alex.lobera@gmail.com>
2020-02-04 16:03:07 +0000

    add comment in ChatContainer.js

--- src/components/Messenger/Chat/ChatContainer.js
+++ src/components/Messenger/Chat/ChatContainer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+// You'll implement the receiveMessages in part 2 question 3
 import { receiveMessages } from "../../../actions";
 import * as api from "../../../api/message";
 import Chat from "./Chat";
