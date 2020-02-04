48c7b33b73bff2c591f3f0adecd5fa54dfd5346a
alexlbr <alex.lobera@gmail.com>
2020-02-04 16:09:14 +0000

    update test-reducer

--- tests/reducers.test.js
+++ tests/reducers.test.js
@@ -3,22 +3,24 @@ import indexReducer from "../src/reducers";
 
 describe("The threadReducer in src/reducers/index.js", () => {
   it("should return the current state if the switch doesn't match any case", () => {
-    const state = threadReducer({ id: 1 }, { type: "test" });
+    const currentState = { id: 1 };
+    const state = threadReducer(currentState, { type: "test" });
 
-    expect(state).toEqual({ id: 1 });
+    expect(state).toEqual(currentState);
   });
 
   it("should set the default state to null", () => {
     const state = threadReducer(undefined, { type: "test" });
 
     expect(state).not.toEqual(undefined);
-    expect(Object.prototype.toString.call(state)).toEqual("[object Null]");
+    expect(state).toEqual(null);
   });
 
   it("should handle the action type called RECEIVE_THREAD and put action.thread in the state", () => {
-    const state = threadReducer([], { type: "RECEIVE_THREAD", thread: [2, 3] });
+    const thread = "dummy message";
+    const state = threadReducer([], { type: "RECEIVE_THREAD", thread });
 
-    expect(state).toEqual([2, 3]);
+    expect(state).toEqual([thread]);
   });
 });
 
