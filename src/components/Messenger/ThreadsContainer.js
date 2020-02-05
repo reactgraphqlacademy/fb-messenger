a4b8a6ba5a7466c6de6165d729c396f08d3731dd
alexlbr <alex.lobera@gmail.com>
2020-02-05 16:23:46 +0000

    update ThreadsContainer.js with compose

--- src/components/Messenger/ThreadsContainer.js
+++ src/components/Messenger/ThreadsContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Threads from "./Threads";
 import { fetchFirstThread } from "../../api/thread";
 import { receiveThread } from "../../actions/thread";
@@ -11,8 +11,16 @@ const compose = (...functions) => initialValue =>
 
 const ThreadsContainer = () => {
   const thread = useSelector(selectThread);
-
+  // Only for options A and C
+  const dispatch = useDispatch();
   useEffect(() => {
+    // Option A
+    fetchFirstThread().then(thread => dispatch(receiveThread(thread)));
+    // Option B
+    fetchFirstThread().then(thread => store.dispatch(receiveThread(thread)));
+    // Option C
+    fetchFirstThread().then(compose(dispatch, receiveThread));
+    // Option D
     fetchFirstThread().then(compose(store.dispatch, receiveThread));
   }, []);
 
